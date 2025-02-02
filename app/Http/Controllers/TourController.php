922d1d1ed3ccb2a3d6b8407dc09fc0650c1c679f
odil <odilorg@gmail.com>
2025-02-02 21:59:56 +0500

    almost done tour page

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -10,11 +10,13 @@ class TourController extends Controller
     public function show($citySlug, $tourSlug)
     {
         // Retrieve the tour where city_slug and slug match
-        $tour = Tour::where('city_slug', $citySlug)
-            ->where('slug', $tourSlug)
-            ->firstOrFail();
-
-        return view('tours.show', compact('tour'));
+        // Retrieve the tour with its relationships
+    $tour = Tour::where('city_slug', $citySlug)
+    ->where('slug', $tourSlug)
+    ->with(['categories', 'tourDays']) // Load relationships
+    ->firstOrFail();
+    $hasMultipleDays = $tour->tourDays->count() > 1;
+return view('tours.show', compact('tour', 'hasMultipleDays'));
     }
 }
 
