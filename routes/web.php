ef4acc73ee58519e4545488d2874116946e11648
Odiljon <odilorg@gmail.com>
2025-02-03 12:42:06 +0500

    about contact done

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,13 @@ use App\Http\Controllers\TourController;
 Route::get('tours-from-{city_slug}/{slug}', [TourController::class, 'show'])->name('tours.show');
 
 Route::get('tours-from-{city_slug}', [AllToursController::class, 'index'])->name('tours.index');
-    
+
+Route::get('about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('contact', function () {
+    return view('contact');
+})->name('contact');
 
 
